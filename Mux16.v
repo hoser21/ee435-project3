74592e097bf4a805609d80041191e0852714ea6a
hoser21 <hoser21@up.edu>
2020-03-26 18:34:26 -0700

    Add mux 16 and 32

---
+++ Mux16.v
@@ -0,0 +1,23 @@
+// ---------------------------------------------------------------------
+//    Module:     1-bit 16:1 Mux
+//    Author:     Kevin Hoser and Alex Schendel
+//    Contact:    hoser21@up.edu and schedel21@up.edu
+//    Date:       03/26/2020
+// ---------------------------------------------------------------------
+
+
+`timescale 1ns / 1ns
+
+module mux16_1 (z, i, s);
+
+output z;
+input [15:0] i;
+input [3:0] s;
+
+wire a, b;
+
+a1 mux8_1(a, i[7:0], s[2:0]);
+a2 mux8_1(b, i[15:8], s[2:0]);
+a3 mux2_1(z, a, b, s[3]);
+
+endmodule
---
+++ Mux32.v
@@ -0,0 +1,23 @@
+// ---------------------------------------------------------------------
+//    Module:     1-bit 32:1 Mux
+//    Author:     Kevin Hoser and Alex Schendel
+//    Contact:    hoser21@up.edu and schedel21@up.edu
+//    Date:       03/26/2020
+// ---------------------------------------------------------------------
+
+
+`timescale 1ns / 1ns
+
+module mux32_1 (z, i, s);
+
+output z;
+input [31:0] i;
+input [4:0] s;
+
+wire a, b;
+
+a1 mux16_1(a, i[15:0], s[3:0]);
+a2 mux16_1(b, i[31:16], s[3:0]);
+a3 mux2_1(z, a, b, s[4]);
+
+endmodule
