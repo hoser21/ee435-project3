c32139e1f267c7373067df0463aefb6c8238c820
Alex Schendel <lexden.s@gmail.com>
2020-03-27 17:07:02 -0700

    Add testbench for Mux32

---
+++ DFF_tb.v
@@ -0,0 +1,41 @@
+// ---------------------------------------------------------------------
+//    Module:     Testbench for D_FF with synchronous reset 
+//    Author:     Nazanin Mansouri
+//    Contact:    mansouri@up.edu
+//    Date:       1/30/2018
+//
+//    All rights reserved
+// ---------------------------------------------------------------------
+
+`timescale 1ns / 1ns
+module test_dff;
+
+reg       clk, reset;
+reg       d;
+wire      q;
+
+D_FF dut (q, d, clk, reset);
+
+initial // Clock generator
+  begin
+    clk = 0;
+    forever #10 clk = !clk;
+  end
+  
+initial	// Test stimulus
+  begin
+    #4 reset = 1;
+    #5 reset = 0;
+    #4 reset = 1;
+    #12  d = 0;
+    #54  d = 1;
+    #21 d = 0;
+    #37 d = 1;
+    #17 d = 0;
+    #48 d = 1;
+  end
+  
+initial
+    $monitor($stime,, reset,, clk,,, d,,, q); 
+    
+endmodule
---
+++ Mux32_tb.v
@@ -0,0 +1,39 @@
+// ---------------------------------------------------------------------
+//    Module:     Testbench for 32:1 Mux
+//    Author:     Alex Schendel and Kevin Hoser
+//    Contact:    schendel21@up.edu and hoser21@up.edu
+//    Date:       3/27/2020
+//
+//    All rights reserved
+// ---------------------------------------------------------------------
+
+`timescale 1ns / 1ns
+
+module test_mux32_1;
+
+reg       [31:0] i
+reg       [4:0] s;
+wire      out;
+
+mux32_1 dut (out, i, s);
+
+  
+initial	// Test stimulus
+  begin
+    #00  i = 32'h00000000; s = 5'b00000;
+    #10  i = 32'hAAAAAAAA; s = 5'b00000;
+    #10  i = 32'h55555555; s = 5'b00001;
+    #10  i = 32'hAAAAAAAA; s = 5'b00001;
+    #10  i = 32'h55555555; s = 5'b00010;
+    #10  i = 32'hAAAAAAAA; s = 5'b11111;
+    #10  i = 32'hAAAAAAAA; s = 5'b11110;
+    #10  i = 32'hFFFFFFFF; s = 5'b00000;
+    #10  i = 32'hFFFFFFFE; s = 5'b00000;
+    #10  i = 32'hFFFFFFFF; s = 5'b11111;
+    #10  i = 32'h7FFFFFFF; s = 5'b11111;
+  end
+  
+initial
+    $monitor($stime,, i0,, i1,,, s,,, out); 
+    
+endmodule  
